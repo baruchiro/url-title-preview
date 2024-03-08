cef4e2673fb07575251b2e016f5e347cc2a0b273
Baruch Odem <baruchiro@gmail.com>
2024-03-08 15:40:00 +0200

    Add activation event and hover provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import axios from "axios";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -24,6 +25,38 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(disposable);
+
+  context.subscriptions.push(
+    vscode.languages.registerHoverProvider(
+      { scheme: "file" },
+      new URLHoverProvider()
+    )
+  );
+}
+
+class URLHoverProvider implements vscode.HoverProvider {
+  private _linkPattern = /(http|https):\/\/[^\s]*\b/g;
+
+  async provideHover(
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ): Promise<vscode.Hover> {
+    const range = document.getWordRangeAtPosition(position, this._linkPattern);
+    if (!range) {
+      // @ts-ignore - docs allow returning null
+      return null;
+    }
+
+    const url = document.getText(range);
+    const title = await this._fetchTitle(url);
+    return new vscode.Hover(`Title: ${title}`);
+  }
+
+  private async _fetchTitle(url: string): Promise<string> {
+    const res = await axios.get(url);
+    const match = res.data.match(/<title>(.*?)<\/title>/i);
+    return match ? match[1] : url;
+  }
 }
 
 // This method is called when your extension is deactivated
