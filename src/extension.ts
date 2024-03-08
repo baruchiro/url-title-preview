c515b04a681d6b19c7a530017cf5de873b5268b7
Baruch Odem <baruchiro@gmail.com>
2024-03-08 15:44:14 +0200

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -8,31 +8,34 @@ import axios from "axios";
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "url-title-preview" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "url-title-preview.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from VSCode!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  console.log('extension "url-title-preview" is now active!');
 
   context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      { scheme: "file" },
-      new URLHoverProvider()
-    )
+    vscode.languages.registerHoverProvider({ scheme: "file" }, { provideHover })
   );
 }
+
+const linkPattern = /(http|https):\/\/[^\s]*\b/g;
+const provideHover = async (
+  document: vscode.TextDocument,
+  position: vscode.Position
+): Promise<vscode.Hover> => {
+  const range = document.getWordRangeAtPosition(position, linkPattern);
+  if (!range) {
+    // @ts-ignore - docs allow returning null
+    return null;
+  }
+
+  const url = document.getText(range);
+  const title = await fetchTitle(url);
+  return new vscode.Hover(`Title: ${title}`);
+};
+
+const fetchTitle = async (url: string): Promise<string> => {
+  const res = await axios.get(url);
+  const match = res.data.match(/<title>(.*?)<\/title>/i);
+  return match ? match[1] : url;
+};
 
 class URLHoverProvider implements vscode.HoverProvider {
   private _linkPattern = /(http|https):\/\/[^\s]*\b/g;
