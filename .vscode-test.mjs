2fb59ce4a7972a29360a8fd7834c1bd0bd2c0faa
Baruch Odem <baruchiro@gmail.com>
2024-03-10 07:32:13 +0200

    Update VSCode test configuration and TypeScript version

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from '@vscode/test-cli';
+import { defineConfig } from "@vscode/test-cli";
+import packageJson from "./package.json" assert { type: "json" };
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+  files: "out/test/**/*.test.js",
+  version: packageJson.engines.vscode.replace(/^\^/, ""),
 });
