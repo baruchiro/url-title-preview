271b7a9da2f437a11bbb18ac5b87325e0c94164f
Baruch Odem <baruchiro@gmail.com>
2024-03-18 16:51:58 +0200

    Add meta-tags functionality
    Close #8

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import axios from "axios";
+import { MetaTags, getMetaTags } from "./meta-tags";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,16 +11,11 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('extension "url-title-preview" is now active!');
 
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider({ scheme: "file" }, { provideHover })
-  );
+  context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: "file" }, { provideHover }));
 }
 
 const linkPattern = /(http|https):\/\/[^\s]*\b/g;
-const provideHover = async (
-  document: vscode.TextDocument,
-  position: vscode.Position
-): Promise<vscode.Hover> => {
+const provideHover = async (document: vscode.TextDocument, position: vscode.Position): Promise<vscode.Hover> => {
   const range = document.getWordRangeAtPosition(position, linkPattern);
   if (!range) {
     // @ts-ignore - docs allow returning null
@@ -27,14 +23,37 @@ const provideHover = async (
   }
 
   const url = document.getText(range);
-  const title = await fetchTitle(url);
-  return new vscode.Hover(`Title: ${title}`);
+  const res = await axios.get(url);
+  const meta = getMetaTags(res.data);
+  const hoverContent = formatHover(meta);
+  console.log(hoverContent.value);
+  return new vscode.Hover(hoverContent);
 };
 
-const fetchTitle = async (url: string): Promise<string> => {
-  const res = await axios.get(url);
-  const match = res.data.match(/<title>(.*?)<\/title>/i);
-  return match ? match[1] : url;
+const formatHover = (meta: MetaTags) => {
+  const lines = [];
+  if (meta.title) {
+    lines.push(`**${meta.title}**`);
+  }
+  if (meta.description) {
+    lines.push(meta.description);
+  }
+  if (meta.image) {
+    const width = calculateLengthOfImage(lines);
+    lines.push(`<img src="${meta.image}" alt="${meta.title}" width="${width}" />`);
+  }
+  const content = new vscode.MarkdownString("$(globe) " + lines.join("  \n"), true);
+  content.supportHtml = true;
+  return content;
+};
+
+const calculateLengthOfImage = (rows: string[]) => {
+  if (rows.length === 0) {
+    return 200;
+  }
+
+  const longestRow = rows.reduce((a, b) => (a.length > b.length ? a : b));
+  return longestRow.length * 7;
 };
 
 // This method is called when your extension is deactivated
